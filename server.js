58d69e775ce003078bb65e5e685f9ba294f92ce7
Артём <legenda333aa@gmail.com>
2025-03-25 22:14:16 +0300

    update

--- server.js
+++ server.js
@@ -149,12 +149,12 @@ router.post('/getInvoiceLink', (req, res) => tgController.getInvoiceLink(req, re
 app.use(express.json());
 app.use(cors());
 app.use('/tg', router);
-//app.listen(port);
+app.listen(port);
 
-bot.launch({
-    webhook: {
-        domain: 'https://mining-empire-backend.vercel.app/',
-        port: 443,
-    },
-});
+//bot.launch({
+//    webhook: {
+ //       domain: 'https://mining-empire-backend.vercel.app/',
+ //       port: 443,
+ //   },
+//});
 
