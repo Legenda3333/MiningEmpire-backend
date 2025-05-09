6905172f6171e9ba67c3956b18cc5cabe49c2dc7
Артём <legenda333aa@gmail.com>
2025-05-09 17:59:12 +0300

    update

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ class TgController {
             .select('*')
             .eq('telegram_id', UserID);    
 
-            res.json({userInfo: NewRegisteredUser, list_friends: friendsList });
+            res.json({userInfo: userData, list_friends: friendsList });
         }
     }
 
